627b660866f1ba5d758d6581e5b65d8eba2722b0
unknown <bibilionyt@gmail.com>
2025-04-04 11:22:54 +0200

    initial setup

---
+++ src/test/java/ch/hadzic/nikola/notesapp/NotesappApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.hadzic.nikola.notesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
