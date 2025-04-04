0962ba010b944b77e520ad2fce0276e2b550387c
Nikola Hadzic <bibilionyt@gmail.com>
2025-04-04 11:45:32 +0200

    setup oauth auth with keycloak and swagger ui config to document apis. added roles to the application

---
+++ src/main/java/ch/hadzic/nikola/notesapp/config/OpenApi30Config.java
@@ -0,0 +1,19 @@
+package ch.hadzic.nikola.notesapp.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "NotesApp", version = "v1.0.0", description = "NotesApp API für das üK Modul 295"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApi30Config {
+
+}
---
+++ src/main/java/ch/hadzic/nikola/notesapp/security/Roles.java
@@ -0,0 +1,9 @@
+package ch.hadzic.nikola.notesapp.security;
+
+public class Roles {
+    public static final String Admin = "admin";
+    public static final String Delete = "delete";
+    public static final String Read = "read";
+    public static final String Update = "update";
+    public static final String Create = "write";
+}
